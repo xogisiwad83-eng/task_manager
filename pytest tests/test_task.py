ca9fc2a63d7671f6866491b628083593c571a119
Шах <mamedov_2000@icloud.com>
2026-02-03 19:02:24 +0400

    поправили тест

--- pytest tests/test_task.py
+++ pytest tests/test_task.py
@@ -5,7 +5,12 @@ from models.task import Task
 from models.enums import Priority, TaskStatus
 
 def test_task_creation():
-    pass
+    task = Task(title="",description="",priority=Priority.MEDIUM)
+    assert task.title == ""
+    assert task.description == ""
+    assert task.priority == Priority.MEDIUM
+    assert task.id is not None
+    assert task.completed is False
 
 def test_task_validation_empty_title():
     with pytest.raises(ValueError):
@@ -25,15 +30,25 @@ def test_task_completion():
 
 def test_task_overdue():
     past_deadline = datetime.now() - timedelta(days=1)
-    future_deadline = datetime.now() + timedelta(days=1)
-    task_past = Task(title= "Past",deadline=past_deadline)
-    task_future = Task(title= "Future",deadline=future_deadline)
-
+    task_past = Task(title="Past", deadline=past_deadline)
     assert task_past is True
+
+    future_deadline = datetime.now() + timedelta(days=1)
+    task_future = Task(title="Future",deadline=future_deadline)
     assert task_future is False
 
 def test_task_update():
-    pass
+    task =  Task(title="", description="", priority=Priority.MEDIUM)
+    task.update(title="  ")
+    assert task.title == "   "
+    assert task.description == ""
+    assert task.priority == Priority.MEDIUM
 
 def test_task_serialization():
-    pass
+    task = Task(title="", description="", priority=Priority.MEDIUM, tags=["optimus", "lana rouds"])
+    data = task.to_dict()
+    restored = task.from_dict(data)
+    assert restored.title == task.title
+    assert restored.description == task.description
+    assert restored.priority == task.priority
+    assert restored.tags == task.tags
